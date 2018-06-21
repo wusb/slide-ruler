802d1cdb6f9439f33b36e68743f6f5bcc0e9b17b
simbawu <code@simbawu.com>
2018-06-21 16:13:07 +0800

    remove react

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppComponent from './page/index/index.js';
-
-ReactDOM.render(
-	<AppComponent />,
-	document.getElementById('app')
-);
