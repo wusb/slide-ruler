1fa5b5d1743c2eecb5fadc50698eeb04a023d68e
simbawu <code@simbawu.com>
2018-06-21 15:51:37 +0800

    last version of react

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppComponent from './page/index/index.js'
+import AppComponent from './page/index/index.js';
 
 ReactDOM.render(
 	<AppComponent />,
