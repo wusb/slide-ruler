49af02b6ba0c9ccbf9d2ffce784ae232a23b24ee
simbawu <code@simbawu.com>
2018-06-22 10:52:28 +0800

    Follow the eslint rules

--- src/slide-ruler.js
+++ src/slide-ruler.js
@@ -40,8 +40,7 @@ class sliderRuler {
   }
 
   _renderBox(container) {
-    const box = document.createElement('div');
-    const canvas = document.createElement('canvas');
+    const box = document.createElement('div'), canvas = document.createElement('canvas');
     this.canvas = canvas;
     box.appendChild(canvas);
     container.appendChild(box);
@@ -49,8 +48,7 @@ class sliderRuler {
   }
 
   _renderCanvas() {
-    const {canvasWidth, canvasHeight} = this.options;
-    const canvas = this.canvas;
+    const {canvasWidth, canvasHeight} = this.options, canvas = this.canvas;
     canvas.width = canvasWidth * 2;
     canvas.height = canvasHeight * 2;
     canvas.style.width = canvasWidth + 'px';
@@ -62,38 +60,35 @@ class sliderRuler {
     this.dreawCanvas();
   }
 
-  touchStart(e){
+  touchStart(e) {
     e.preventDefault();
-    if(e.touches.length == 1 || this.localState.isTouchEnd){
+    if (e.touches.length === 1 || this.localState.isTouchEnd) {
       this.touchPoints(e);
       let touch = e.touches[0];
       console.log(e);
       this.localState.startX = touch.pageX;
       this.localState.startY = touch.pageY;
-      this.localState.startT = new Date().getTime(); //记录手指按下的开始时间
-      this.localState.isMove = false; //是否产生滑动
-      this.localState.isTouchEnd = false; //当前开始滑动
+      this.localState.startT = new Date().getTime(); // 记录手指按下的开始时间
+      this.localState.isMove = false; // 是否产生滑动
+      this.localState.isTouchEnd = false; // 当前开始滑动
     }
   }
 
-  touchMove(e){
+  touchMove(e) {
     this.touchPoints(e);
-    let touch = e.touches[0];
-    let deltaX = touch.pageX - this.localState.startX;
-    let deltaY = touch.pageY - this.localState.startY;
+    let touch = e.touches[0],
+      deltaX = touch.pageX - this.localState.startX,
+      deltaY = touch.pageY - this.localState.startY;
     console.log('deltaX:', e);
-    //如果X方向上的位移大于Y方向，则认为是左右滑动
-    if (Math.abs(deltaX) > Math.abs(deltaY)){
+    // 如果X方向上的位移大于Y方向，则认为是左右滑动
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
       this.moveDreaw(deltaX);
       this.localState.isMove = true;
     }
   }
 
-  touchEnd(e){
+  touchEnd(e) {
     console.log(e);
-    // 计算手指在屏幕上停留的时间
-    let touch = e;
-    console.log(touch);
     const {divide} = this.options;
     console.log('currentValue1:', this.options.currentValue);
 
@@ -108,17 +103,17 @@ class sliderRuler {
   }
 
   touchPoints(e) {
-    let touch = e.touches[0];
-    let time = new Date().getTime();
-    let shift = touch.pageX;
-    this.localState.touchPoints.push({time: time, shift: shift})
+    let touch = e.touches[0],
+      time = new Date().getTime(),
+      shift = touch.pageX;
+    this.localState.touchPoints.push({time: time, shift: shift});
   }
 
   inertialShift() {
     let s = 0;
-    if(this.localState.touchPoints.length >= 4) {
-      let _points = this.localState.touchPoints.slice(-4);
-      let v = ((_points[3].shift - _points[0].shift) / (_points[3].time - _points[0].time)) * 1000; // v 手指离开屏幕后的速度px/s
+    if (this.localState.touchPoints.length >= 4) {
+      let _points = this.localState.touchPoints.slice(-4),
+        v = ((_points[3].shift - _points[0].shift) / (_points[3].time - _points[0].time)) * 1000; // v 手指离开屏幕后的速度px/s
       const a = 60000; // a 手指离开屏幕后的加速度
       let s = Math.sign(v) * Math.pow(v, 2) / (2 * a); // s 手指离开屏幕后惯性距离
       console.log(_points, _points[3].shift - _points[0].shift, _points[3].time - _points[0].time, v, s);
@@ -130,26 +125,25 @@ class sliderRuler {
     const {divide} = this.options;
     let moveValue = Math.round(-shift / divide);
     console.log('moveValue:', Math.abs(moveValue));
-    for (let i = 0; i<= Math.abs(moveValue); i++){
+    for (let i = 0; i <= Math.abs(moveValue); i++) {
       this.options.currentValue += Math.sign(moveValue);
       window.requestAnimationFrame(this.dreawCanvas.bind(this));
     }
   }
 
   dreawCanvas() {
-    const canvas = this.canvas;
-    const context = canvas.getContext('2d');
+    const canvas = this.canvas,
+      context = canvas.getContext('2d');
     canvas.height = canvas.height;
     let {containerWidth, canvasHeight, maxValue, minValue, currentValue, precision, divide, heightDecimal, heightDigit, lineWidth, colorDecimal, colorDigit, fontSize, fontColor} = this.options;
-    currentValue = currentValue > minValue ? ( currentValue < maxValue ? currentValue : maxValue) : minValue;
+    currentValue = currentValue > minValue ? (currentValue < maxValue ? currentValue : maxValue) : minValue;
     this.options.currentValue = currentValue;
     // 1.1 定义原点，x轴方向起点与终点各留半屏空白
-    let diffCurrentMin = (currentValue - minValue) * divide;
-    let origin = {x: diffCurrentMin > containerWidth/2 ? 0 : ( containerWidth/2 - diffCurrentMin ) * 2, y: canvasHeight * 2};
-
-    let startValue = currentValue - containerWidth/2/divide * precision;
-    startValue = startValue > minValue ? ( startValue < maxValue ? startValue : maxValue) : minValue;
-    let endValue = startValue + containerWidth/divide * precision;
+    let diffCurrentMin = (currentValue - minValue) * divide,
+      origin = {x: diffCurrentMin > containerWidth / 2 ? 0 : (containerWidth / 2 - diffCurrentMin) * 2, y: canvasHeight * 2},
+      startValue = currentValue - containerWidth / 2 / divide * precision;
+    startValue = startValue > minValue ? (startValue < maxValue ? startValue : maxValue) : minValue;
+    let endValue = startValue + containerWidth / divide * precision;
     endValue = endValue < maxValue ? endValue : maxValue;
     // 1.2 定义刻度线样式
     heightDecimal = heightDecimal * 2;
@@ -164,30 +158,30 @@ class sliderRuler {
     // 1.6定义每个刻度的精度
     const derivative = 1 / precision;
 
-    for (let i = startValue/precision; i <= endValue/precision; i++) {
+    for (let i = startValue / precision; i <= endValue / precision; i++) {
       context.beginPath();
       // 2.2 画刻度线
-      context.moveTo(origin.x + (i - startValue/precision) * divide, 0);
+      context.moveTo(origin.x + (i - startValue / precision) * divide, 0);
       // 画线到刻度高度，10的位数就加高
-      context.lineTo(origin.x + (i - startValue/precision) * divide, i % 10 == 0 ? heightDecimal : heightDigit);
+      context.lineTo(origin.x + (i - startValue / precision) * divide, i % 10 === 0 ? heightDecimal : heightDigit);
       context.lineWidth = lineWidth;
       // 10的位数就加深
-      context.strokeStyle = (i % 10 == 0) ? colorDecimal : colorDigit;
+      context.strokeStyle = (i % 10 === 0) ? colorDecimal : colorDigit;
       context.stroke();
-      //描绘刻度值
+      // 描绘刻度值
       context.fillStyle = fontColor;
-      context.textAlign = "center";
-      context.textBaseline = "top";
-      if (i % 10 == 0) {
+      context.textAlign = 'center';
+      context.textBaseline = 'top';
+      if (i % 10 === 0) {
         context.font = `${fontSize}px Arial`;
-        context.fillText(Math.round(i / 10) / (derivative / 10), origin.x + (i - startValue/precision) * divide, heightDecimal);
+        context.fillText(Math.round(i / 10) / (derivative / 10), origin.x + (i - startValue / precision) * divide, heightDecimal);
       }
       context.closePath();
     }
   }
 
   init(options) {
-    this._renderBox(options.el)
+    this._renderBox(options.el);
   }
 }
 
