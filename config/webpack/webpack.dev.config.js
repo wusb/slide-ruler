a8022d104c39d6abd0c5c4b0a4fc33aa17160e40
simbawu <code@simbawu.com>
2018-06-25 18:29:33 +0800

    change dev publicPath

--- config/webpack/webpack.dev.config.js
+++ config/webpack/webpack.dev.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../../docs'),
-    publicPath: '/docs/'
+    publicPath: '/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
