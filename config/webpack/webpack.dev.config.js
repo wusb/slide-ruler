1f9942955e0b1a05f7f80d2f78f7e946b6fcd212
simbawu <code@simbawu.com>
2018-06-25 18:35:22 +0800

    change publicPath

--- config/webpack/webpack.dev.config.js
+++ config/webpack/webpack.dev.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../../docs'),
-    publicPath: '/'
+    publicPath: ''
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
--- docs/demo.js
+++ docs/demo.js
@@ -63,7 +63,7 @@
 /******/ 	}
 /******/
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "bfca2ed15bc150a4b754"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "83144510e4f581f4e1de"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -779,7 +779,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
