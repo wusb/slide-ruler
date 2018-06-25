87cbecb612c632782c163f8bb9e4f3a79b2f33f8
simbawu <code@simbawu.com>
2018-06-25 17:28:42 +0800

    add new demo

--- src/demo.js
+++ src/demo.js
@@ -1,5 +1,26 @@
-import SlideRuler from "./slide-ruler";
+import DemoComponent from './demo-component';
 
-new SlideRuler({
-  el: document.querySelector('#demo')
+new DemoComponent({
+  typeName: '年龄',
+  unit: '年',
+  maxValue: 2018,
+  minValue: 1938,
+  currentValue: 1996
+});
+
+new DemoComponent({
+  typeName: '身高',
+  unit: 'cm',
+  maxValue: 230,
+  minValue: 100,
+  currentValue: 180
+});
+
+new DemoComponent({
+  typeName: '体重',
+  unit: 'kg',
+  maxValue: 200,
+  minValue: 30,
+  currentValue: 76.1,
+  precision: 0.1
 });
