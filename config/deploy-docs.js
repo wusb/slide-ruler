bb7fe926edc8c5b9d0c52661a2b9f814e73cddda
simbawu <code@simbawu.com>
2018-06-27 15:46:20 +0800

    add deploy script

---
+++ config/deploy-docs.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack'),
+  webpackConfig = require('./webpack/webpack.config'),
+  simpleGit = require('simple-git');
+
+new Promise((resolve, reject) => {
+  webpack({ mode: 'development',
+    ...webpackConfig
+  }).run((err, stats) => {
+    if (err) {
+      reject(err);
+    } else {
+      console.log(stats.toString(webpackConfig.stats));
+      resolve();
+    }
+  });
+}).then(() => {
+  console.log('deploy docs start');
+  simpleGit.add('.')
+    .commit('Update docs pages')
+    .push(['origin', 'master'], () => console.log('deploy docs done'));
+});
